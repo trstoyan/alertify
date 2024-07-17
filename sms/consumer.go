1cf81bc41eb5b208dfa91eb5d9d6ce961eaa2505
trstoyan <stoyantr@icloud.com>
2024-07-17 10:40:26 +0300

    chore: Add SMS consumer functionality to handle incoming messages

---
+++ sms/consumer.go
@@ -0,0 +1,36 @@
+package sms
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/trstoyan/alertify/kafka"
+)
+
+type SMSPayload struct {
+	PhoneNumber string `json:"phone_number"`
+	Message     string `json:"message"`
+}
+
+func StartSMSConsumer(brokerAddress, topic, groupID string) {
+	consumer := kafka.NewKafkaConsumer(brokerAddress, topic, groupID)
+	go kafka.ConsumeMessages(consumer, handleSMSMessage)
+}
+
+func handleSMSMessage(msg kafka.Message) {
+	var payload SMSPayload
+	err := json.Unmarshal(msg.Value, &payload)
+	if err != nil {
+		log.Printf("error unmarshalling message: %v", err)
+		return
+	}
+
+	err = SendSMS(payload.PhoneNumber, payload.Message)
+	if err != nil {
+		log.Printf("error sending SMS: %v", err)
+		return
+	}
+
+	log.Printf("SMS sent to %s: %s", payload.PhoneNumber, payload.Message)
+}
