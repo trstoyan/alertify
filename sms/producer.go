b57d5d4840627af4424d26c2ed87099689acd2d0
trstoyan <stoyantr@icloud.com>
2024-07-17 10:40:31 +0300

    chore: Add SMS producer functionality to send messages

---
+++ sms/producer.go
@@ -0,0 +1,16 @@
+package sms
+
+import (
+	"encoding/json"
+
+	"github.com/trstoyan/alertify/kafka"
+)
+
+func ProduceSMSMessage(producer *kafka.Writer, payload SMSPayload) error {
+	msg, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	return kafka.ProduceMessage(producer, nil, msg)
+}
