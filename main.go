5f3c439d4d108b0d57cec8b4413f5e427443542e
trstoyan <stoyantr@icloud.com>
2024-07-17 10:39:23 +0300

    chore: Add SMS functionality to main.go file

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/trstoyan/alertify/kafka"
+	"github.com/trstoyan/alertify/sms"
+)
+
+func main() {
+	brokerAddress := "localhost:9092"
+	smsTopic := "sms_topic"
+	groupID := "sms_service"
+
+	// Start SMS Consumer
+	sms.StartSMSConsumer(brokerAddress, smsTopic, groupID)
+
+	// Simulate producing an SMS message
+	producer := kafka.NewKafkaProducer(brokerAddress, smsTopic)
+	payload := sms.SMSPayload{
+		PhoneNumber: "1234567890",
+		Message:     "Hello, this is a test message.",
+	}
+	err := sms.ProduceSMSMessage(producer, payload)
+	if err != nil {
+		log.Fatalf("failed to produce SMS message: %v", err)
+	}
+}
