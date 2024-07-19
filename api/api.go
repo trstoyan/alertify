fe9ffbff2bca85956ea0a7d350d7bb34ba91f302
trstoyan <stoyantr@icloud.com>
2024-07-19 12:26:23 +0300

    chore: Refactor API code and add Notification struct

--- api/api.go
+++ api/api.go
@@ -1,19 +1,7 @@
 package api
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-
-	"github.com/trstoyan/alertify/api/handlers"
-)
-
-func main() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/notify", handlers.NotificationHandler).Methods("POST")
-
-	fmt.Println("Listening on port :8080")
-	http.ListenAndServe(":8080", router)
+type Notification struct {
+	ID      string `json:"id"`
+	Channel string `json:"channel"`
+	Message string `json:"message"`
 }
