150ea7710c0519c2b9edb568bc7fe4618a49c8ab
trstoyan <stoyantr@icloud.com>
2024-07-19 12:26:18 +0300

    chore: Add Kafka consumer for sending SMS and email notifications

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"github.com/trstoyan/alertify/api"
+	"github.com/trstoyan/alertify/email"
+	"github.com/trstoyan/alertify/kafka"
+	"github.com/trstoyan/alertify/sms"
+
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func init() {
-	fmt.Println("Initializing...")
-}
-
 func main() {
-	fmt.Println("Hello, World!")
+	go api.StartServer()
+
+	go kafka.Consume("sms-topic", sms.SendSMS)
+	go kafka.Consume("email-topic", email.SendEmail)
+
+	// Graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Shutting down gracefully...")
 }
