aa3fae024f07ea2c63897d03d5b3fbfa1708cd96
trstoyan <stoyantr@icloud.com>
2024-07-19 12:27:09 +0300

    chore: Add Kafka consumer for handling SMS message consumption

--- sms/consumer.go
+++ sms/consumer.go
@@ -1,1 +1,31 @@
 package sms
+
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func ConsumeSMS() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "10.25.8.39:9092",
+		"group.id":          "sms-consumer",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %s", err)
+	}
+	defer c.Close()
+
+	c.SubscribeTopics([]string{"sms-topic"}, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			SendSMS(string(msg.Value))
+			log.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			log.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+}
