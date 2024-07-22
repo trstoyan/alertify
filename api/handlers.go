efb7b261510135694c2248d17b9904cb0a7c400b
trstoyan <stoyantr@icloud.com>
2024-07-22 07:59:30 +0300

    refactor: Update Kafka consumer functions to handle message consumption

--- api/handlers.go
+++ api/handlers.go
@@ -19,16 +19,38 @@ func ValidateNotification(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid notification payload", http.StatusBadRequest)
 		return
 	}
-	if notification.Channel == "sms" {
-		sms.ProduceSMS(notification.Message)
-		log.Printf("SMS notification received: %+v\n", notification)
-	}
-	if notification.Channel == "email" {
-		email.ProduceEmail(notification.Message)
-		log.Printf("Email notification received: %+v\n", notification)
-	}
-	// Add routing to the appropriate channel service here
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Notification received"))
 
+	// Initialize a variable to hold the response message
+	var response struct {
+		Message string `json:"message"`
+		Status  string `json:"status"`
+	}
+
+	if notification.Channel == "sms" {
+		msgKey := sms.ProduceSMS(notification.Message)
+		log.Printf("SMS notification received: %+v\n", notification)
+		response.Message = msgKey
+
+	} else if notification.Channel == "email" {
+		msgKey := email.ProduceEmail(notification.Message)
+		response.Message = msgKey
+
+		log.Printf("Email notification received: %+v\n", notification)
+
+	} else {
+		http.Error(w, "Invalid notification channel", http.StatusBadRequest)
+		return
+	}
+
+	// Set Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	// Encode the response as JSON and send it
+	json.NewEncoder(w).Encode(response)
+}
+
+func HandleResponse(key, status string) {
+
+	// Here you can handle the email response, for example, log it or store it in a database
+	log.Printf("Received email response for key %s: %s", key, status)
 }
