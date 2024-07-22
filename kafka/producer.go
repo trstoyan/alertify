b6c00b57ae8d0742eb9a278f8a2978725f4859dc
trstoyan <stoyantr@icloud.com>
2024-07-22 08:00:01 +0300

    refactor: Update Kafka producer to include message key and delivery channel
    This commit updates the `ProduceWithKey` function in the `kafka/producer.go` file to include the message key and a delivery channel. The message key is used to uniquely identify the message, and the delivery channel is used to handle delivery reports for each produced message.
    
    By including the message key, the application can ensure that messages with the same key are always sent to the same partition in the Kafka topic. This helps maintain message order and improves the efficiency of message processing.
    
    The addition of the delivery channel allows the application to handle delivery reports for each produced message. This ensures that the application can track the success or failure of message delivery and take appropriate action if needed.
    
    These changes enhance the functionality of the Kafka producer by providing more control over message routing and delivery, improving the reliability and performance of the application.

--- kafka/producer.go
+++ kafka/producer.go
@@ -6,20 +6,47 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func Produce(topic string, message string) {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "10.25.8.39:9092"})
+func ProduceWithKey(topic, key, message string) error {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092",
+		"security.protocol": "SASL_SSL",
+		"sasl.mechanisms":   "PLAIN",
+		"sasl.username":     "UQLV7MXX2G4BMN4G",
+		"sasl.password":     "tVNWl/C5NIUPTc+eHRk4U3cJbBW4lcvv89GkbZ1sBCkFOemlt1sWerbBfyQ2k7oQ",
+		"retries":           5,   // Retry up to 5 times
+		"retry.backoff.ms":  500, // Wait for 500ms before retrying
+	})
 	if err != nil {
 		log.Fatalf("Failed to create producer: %s", err)
+		return err // Return error instead of terminating the program
 	}
 	defer p.Close()
 
+	deliveryChan := make(chan kafka.Event, 10000) // Use a delivery channel to handle reports
+
+	// Produce message
 	err = p.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            []byte(key),
 		Value:          []byte(message),
-	}, nil)
+	}, deliveryChan)
 	if err != nil {
-		log.Printf("Failed to produce message: %s", err)
+		return err // Return error if message production fails
 	}
 
-	p.Flush(5 * 1000)
+	go func() {
+		for e := range deliveryChan {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Failed to deliver message: %v\n", ev.TopicPartition.Error)
+				}
+			}
+		}
+	}()
+
+	// Wait for all messages to be delivered
+	p.Flush(15 * 1000) // Adjust the timeout as needed
+
+	return nil
 }
