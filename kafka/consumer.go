c9c7423b8ccdd4ed6d7af5aa308c48dd450427c2
trstoyan <stoyantr@icloud.com>
2024-07-22 07:59:54 +0300

    refactor: Update Kafka consumer to handle message consumption with worker goroutines
    This commit updates the `Consume` function in the `kafka/consumer.go` file to handle message consumption using worker goroutines. It creates a channel for messages and starts multiple worker goroutines to process the messages concurrently. Each worker goroutine receives a Kafka message from the channel, calls the `handleMessage` callback function, and logs the result. This approach improves the efficiency and scalability of message processing in the application.
    
    The consumer is also now configured with the necessary Kafka properties, such as the bootstrap servers, SASL authentication, and group ID.
    
    These changes enhance the functionality of the application by enabling efficient and concurrent message consumption from Kafka for further processing.

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -6,25 +6,63 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func Consume(handleMessage func(string), topic string) {
+func Consume(handleMessage func(string, string) string, topic string) {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "10.25.8.39:9092",
-		"group.id":          "notification-consumer",
-		"auto.offset.reset": "earliest",
-	})
+		"bootstrap.servers": "pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092",
+		"sasl.username":     "UQLV7MXX2G4BMN4G",
+		"sasl.password":     "tVNWl/C5NIUPTc+eHRk4U3cJbBW4lcvv89GkbZ1sBCkFOemlt1sWerbBfyQ2k7oQ",
+		"security.protocol": "SASL_SSL",
+		"sasl.mechanisms":   "PLAIN",
+		"group.id":          "kafka-go-getting-started",
+		"auto.offset.reset": "earliest"})
+	// Check for errors when creating the consumer and connect to the Kafka cluster
 	if err != nil {
 		log.Fatalf("Failed to create consumer: %s", err)
 	}
+
+	type KafkaMessage struct {
+		Key   string
+		Value string
+	}
+	// Close the consumer when the function returns
 	defer c.Close()
 
-	c.SubscribeTopics([]string{topic}, nil)
+	err = c.SubscribeTopics([]string{topic}, nil)
+	// Check for errors when subscribing to topics
+	if err != nil {
+		log.Fatalf("Failed to subscribe to topics: %s", err)
+	}
 
+	// Create a channel for messages
+	messageChan := make(chan KafkaMessage)
+
+	// Number of worker goroutines
+	numWorkers := 30
+
+	// Start worker goroutines
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			for kafkaMsg := range messageChan {
+				responseMessage := handleMessage(kafkaMsg.Key, kafkaMsg.Value)
+				if responseMessage == "" {
+					log.Printf("Failed to handle message: %s", responseMessage)
+				} else {
+					log.Printf("Message handled successfully: %s", responseMessage)
+				}
+			}
+		}()
+	}
+
+	// Main loop to read messages and send them to the channel
 	for {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
-			handleMessage(string(msg.Value))
+			messageChan <- KafkaMessage{Key: string(msg.Key), Value: string(msg.Value)} // Send message to worker goroutines only if there's a message
 		} else {
+			// Handle the error, e.g., log it or implement a retry mechanism
 			log.Printf("Consumer error: %v (%v)\n", err, msg)
 		}
 	}
+
+	// close(messageChan)
 }
