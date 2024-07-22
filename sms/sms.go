ea38a9afb4e620fd07cd155b3c03738e7704b25a
trstoyan <stoyantr@icloud.com>
2024-07-22 10:33:39 +0300

    refactor: Update SMS package to use Kafka for message delivery
    This commit updates the `sms.go` file in the SMS package to integrate with the Kafka messaging system for message delivery. It adds the necessary import statement for the `kafka` package and modifies the `SendSMS` function to produce the message to the Kafka topic using the `ProduceWithKey` function.
    
    By leveraging Kafka for message delivery, the application can benefit from its scalability, fault-tolerance, and reliability. This update enhances the SMS functionality by ensuring that messages are reliably produced to the Kafka topic, allowing for efficient and reliable message processing.
    
    These changes improve the overall performance and reliability of the SMS functionality by utilizing Kafka for message delivery.

--- sms/sms.go
+++ sms/sms.go
@@ -2,6 +2,9 @@ package sms
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/trstoyan/alertify/kafka"
 )
 
 func SendSMS(key, message string) string {
@@ -9,7 +12,14 @@ func SendSMS(key, message string) string {
 	// Replace with actual SMS gateway API call
 	fmt.Println("Sending SMS:", message, "with key:", key)
 
-	responseMessage := "SMS sent successfully"
+	responseMessage := SendSMSTwilio("+19382531802", "+359884390195", message)
 
-	return responseMessage
+	err := kafka.ProduceWithKey(responseTopic, key, *responseMessage.Body)
+	if err != nil {
+		log.Printf("Failed to produce email response: %s", err)
+	} else {
+		log.Printf("Produced email response for key: %s", key)
+	}
+
+	return *responseMessage.Body
 }
