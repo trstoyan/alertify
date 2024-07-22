39e25dfef063fad4a798a536a3c6ed0a47f16a4d
trstoyan <stoyantr@icloud.com>
2024-07-22 07:59:48 +0300

    feat: Update email producer to include message key and response topic

--- email/producer.go
+++ email/producer.go
@@ -1,10 +1,37 @@
 package email
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+	"time"
+
 	"github.com/trstoyan/alertify/kafka"
 )
 
-func ProduceEmail(message string) {
-	kafka.Produce(message, "email-topic")
-	// Note: In a real application, ensure proper shutdown logic to drain the Events channel and close the producer cleanly.
+const topic string = "email-topic"
+
+func ProduceEmail(message string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(message + time.Now().String()))
+	msgKey := hex.EncodeToString(hasher.Sum(nil))
+
+	err := kafka.ProduceWithKey(topic, msgKey, message)
+	if err != nil {
+		log.Fatalf("Failed to send Email: %s", err)
+	} else {
+		log.Printf("Email sent successfully with key: %s\n", msgKey)
+	}
+	return msgKey
+}
+
+const responseTopic string = "email-sent-topic"
+
+func ProduceSMSResponse(message string) {
+	err := kafka.ProduceWithKey(responseTopic, "", message)
+	if err != nil {
+		log.Fatalf("Failed to send Email response: %s", err)
+	} else {
+		log.Printf("Email response sent successfully with key: %s\n", message)
+	}
 }
