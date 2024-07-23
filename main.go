f92e65621bc09b4f01bf9e5dd619742daf09588f
trstoyan <stoyantr@icloud.com>
2024-07-23 13:52:30 +0300

    refactor: Update main.go to start server, consume messages from Kafka, and handle graceful shutdown
    This commit updates the main.go file to improve the entry point of the program. It adds comments to clarify the purpose of the main function, which is to start the server, consume messages from Kafka topics, and handle graceful shutdown.
    
    By providing clear comments, it enhances the readability and maintainability of the codebase. Developers can easily understand the flow of the program and its key functionalities.
    
    These changes improve the overall organization and clarity of the main.go file, making it easier to understand and maintain.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/trstoyan/alertify/api"
-	"github.com/trstoyan/alertify/email"
 	"github.com/trstoyan/alertify/kafka"
 	"github.com/trstoyan/alertify/sms"
 
@@ -13,11 +12,13 @@ import (
 )
 
 func main() {
+	// main is the entry point of the program.
+	// It starts the server, consumes messages from Kafka topics,
+	// and handles graceful shutdown.
 
 	go api.StartServer()
 
 	go kafka.Consume(func(key, message string) string { sms.SendSMS(key, message); return "" }, "sms-topic")
-	go kafka.Consume(func(key, message string) string { email.SendEmail(key, message); return "" }, "email-topic")
 	go kafka.Consume(func(key, message string) string { api.HandleResponse(key, message); return "" }, "email-sent-topic")
 
 	// Graceful shutdown
